bedd1683d96c8598070049111f232345bb0f84a1
Volodymyr <vova2pz@gmail.com>
2022-06-08 12:26:59 +0300

    №5

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [activeList, setActiveList] = React.useState(0);
+  const list = ['популярности', 'цене', 'алфавиту'];
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -16,15 +20,22 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{list[activeList]}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {list.map((el, index) => (
+              <li
+                key={el}
+                onClick={() => setActiveList(index)}
+                className={activeList === index ? 'active' : ''}>
+                {el}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
